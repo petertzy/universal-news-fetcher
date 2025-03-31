3b77193ffb7942f5a9257955071bb17e5e424016
petertzy <obitzy@gmx.de>
2025-03-31 18:52:16 +0200

    Update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = FastAPI()
 # Configure CORS to allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from frontend
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all request headers
