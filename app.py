8b47a9901c1f4c154e5cb411f5d615cd28ebaefa
petertzy <obitzy@gmx.de>
2025-03-31 18:56:00 +0200

    Update

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ app = FastAPI()
 # Configure CORS to allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=["*"],
-    allow_origins=["https://pushnotificatinbackend.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods
     allow_headers=["*"],  # Allow all request headers
